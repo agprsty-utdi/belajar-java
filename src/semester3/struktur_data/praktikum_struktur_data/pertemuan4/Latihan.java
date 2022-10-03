f2ef9654a52797982c7f2e5bcba7a9daf538515b
agung <agungprsty423@gmail.com>
2022-10-03 22:30:32 +0700

    Menambahkan tugas praktik pertemuan 4

---
+++ src/semester3/struktur_data/praktikum_struktur_data/pertemuan4/Latihan.java
@@ -0,0 +1,121 @@
+package semester3.struktur_data.praktikum_struktur_data.pertemuan4;
+
+import java.util.Scanner;
+
+class formatBiodata { 
+    //bagian deklarasi struktur record
+    String nama;
+    String alamat;
+    int umur;
+    char jekel;
+    String hobi[] = new String[3];
+    float ipk;
+}
+
+public class Latihan {
+    public static int N=3;
+    //--------------------------------------------------
+    //--- Fungsi untuk mengentri data ke dalam Larik ---
+    //--------------------------------------------------
+    public static void ngentriData(formatBiodata biodataMahasiswa[])
+    {
+        Scanner masukan = new Scanner(System.in);
+        int bacaTombol=0;
+        
+        for (int i=0; i<=N-1; i++)
+        {
+            System.out.print("Silakan masukkan nama anda : ");
+            biodataMahasiswa[i].nama = masukan.next();
+            System.out.print("Silakan masukkan alamat anda : ");
+            biodataMahasiswa[i].alamat = masukan.next();
+            System.out.print("Silakan masukkan umur anda : ");
+            biodataMahasiswa[i].umur = masukan.nextInt();
+            System.out.print("Silakan masukkan Jenis Kelamin anda : "); 
+            try
+            { 
+                bacaTombol = System.in.read();
+            }
+            catch(java.io.IOException e){}
+
+            biodataMahasiswa[i].jekel = (char)bacaTombol;
+            System.out.println("Silakan masukkan hobi (maks 3) : ");
+            System.out.print("hobi ke-0 : ");
+            biodataMahasiswa[i].hobi[0] = masukan.next();
+            System.out.print("hobi ke-1 : ");
+            biodataMahasiswa[i].hobi[1] = masukan.next();
+            System.out.print("hobi ke-2 : ");
+            biodataMahasiswa[i].hobi[2] = masukan.next();
+            System.out.print("Silakan masukkan IPK anda : ");
+            biodataMahasiswa[i].ipk = masukan.nextFloat();
+        }
+    }
+
+    //------------------------------------------------------------------
+    //--- Fungsi untuk Mencari Data Secara Linear Search (Loop: For) ---
+    //------------------------------------------------------------------
+    
+    public static void mencariData(formatBiodata biodataMahasiswa[])
+    { 
+        String kataKunci;
+        //bagian memasukkan kata kunci ------------------------------------------
+        Scanner masukan = new Scanner(System.in);
+        System.out.print("Masukkan kata kunci jekel (l/p) : ");
+        kataKunci = masukan.next();
+        
+        //bagian mencari data satu persatu urut dari larik terdepan
+        for (int i=0; i<= N-1; i++)
+        {
+            char myCharFromKataKunci = kataKunci.charAt(0);
+            if (biodataMahasiswa[i].jekel == myCharFromKataKunci){ 
+                System.out.print (i +".");
+                System.out.print (biodataMahasiswa[i].nama + "\t");
+                System.out.print (biodataMahasiswa[i].alamat + "\t");
+                System.out.print (biodataMahasiswa[i].umur + "\t");
+                System.out.print (biodataMahasiswa[i].jekel + "\t");
+                System.out.print (biodataMahasiswa[i].hobi[0] + "\t");
+                System.out.print (biodataMahasiswa[i].hobi[1] + "\t");
+                System.out.print (biodataMahasiswa[i].hobi[2] + "\t");
+                System.out.println(biodataMahasiswa[i].ipk);
+            }
+        }
+    }
+
+    //--------------------------------------------------
+    //--- Fungsi untuk Menampilkan Data ---
+    //--------------------------------------------------
+    public static void tampilkanData(formatBiodata biodataMahasiswa[])
+    {
+        //bagian menampilkan isi struktur Larik -------
+        System.out.println("-------------------------------------------------------");
+        System.out.println("NAMA ALAMAT UMUR JEKEL HOBI[0] HOBI[1] HOBI[2] IPK");
+        System.out.println("-------------------------------------------------------");
+        for (int i=0; i<=N-1; i++)
+        { 
+            System.out.print (i +".");
+            System.out.print (biodataMahasiswa[i].nama + "\t");
+            System.out.print (biodataMahasiswa[i].alamat + "\t");
+            System.out.print (biodataMahasiswa[i].umur + "\t");
+            System.out.print (biodataMahasiswa[i].jekel + "\t");
+            System.out.print (biodataMahasiswa[i].hobi[0] + "\t");
+            System.out.print (biodataMahasiswa[i].hobi[1] + "\t");
+            System.out.print (biodataMahasiswa[i].hobi[2] + "\t");
+            System.out.println(biodataMahasiswa[i].ipk);
+        }
+        System.out.println("-------------------------------------------------------");
+    }
+
+    //--------------------------------------------------
+    //--- Program Utama ---
+    //--------------------------------------------------
+    public static void main(String[] args)
+    { 
+        //bagian deklarasi record berbasis LARIK
+        formatBiodata biodataMahasiswa[] = new formatBiodata[4];
+        biodataMahasiswa[0] = new formatBiodata();
+        biodataMahasiswa[1] = new formatBiodata();
+        biodataMahasiswa[2] = new formatBiodata();
+        ngentriData(biodataMahasiswa);
+        tampilkanData(biodataMahasiswa);
+        mencariData(biodataMahasiswa); // Mencari data berdasarkan keyword jekel l/p
+    }
+}
